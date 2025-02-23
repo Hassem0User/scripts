c07564ceee7cebbcd260439f50635b09c86f4c84
Hassem0User <ha-aechegaray@hotmail.com>
2025-02-23 15:57:15 +0100

    added commnets

--- ss.sh
+++ ss.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Common nmap command for a first interaction
+# -p- scan all ports
+# --open look only for open ports
+# -sS stealth scan
+# --min-rate should be adjusted according to machine potency
+# -vvv add triple verbose
+# -Pn dont ping hosts
+# -og export to a grep file
+
 sudo nmap -p- --open -sS --min-rate 5000 -vvv -Pn $1 -oG sscan
 
